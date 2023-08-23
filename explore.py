ed0b047a0dd51b11a4ab94133936e2cba8f54c16
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-08-23 16:59:44 -0500

    Docs and comments

--- explore.py
+++ explore.py
@@ -1,66 +1,18 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
+from sklearn.metrics import mutual_info_score
+import matplotlib.pyplot as plt
+import prepare as p
+import explore as e
 
-def explore_cat(train, target, cat_var):
-    """
-    Explore the relationship between a binary target variable and a categorical variable.
+plt.style.use(
+    "https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle"
+)
 
-    Parameters:
-    train (pandas.DataFrame): The training data.
-    target (str): The name of the binary target variable.
-    cat_var (str): The name of the categorical variable to explore.
 
-    Returns:
-    None
-    """
-    # Calculate the observed contingency table and perform a chi-squared test
-    observed = pd.crosstab(train[cat_var], train[target])
-    chi2, p, degf, expected = stats.chi2_contingency(observed)
-    print(f'Chi2: {chi2}, p-value: {p}')
-    
-    # Create a vertical bar plot of the categorical variable split by the target variable
-    plt.figure(figsize=(4,4))
-    sns.barplot(x=target, y=cat_var, data=train, alpha=.8)
-    
-    # Add a horizontal line to the plot at the overall rate of the target variable
-    overall_rate = train[target].mean()
-    plt.axvline(overall_rate, ls='--')
-    
-    # Show the plot
-    plt.show()
-
-def explore_int(train, target, cat_var):
-    """
-    Explore the relationship between a binary target variable and an integer variable.
-
-    Parameters:
-    train (pandas.DataFrame): The training data.
-    target (str): The name of the binary target variable.
-    cat_var (str): The name of the integer variable to explore.
-
-    Returns:
-    None
-    """
-    # Calculate the observed contingency table and perform a chi-squared test
-    observed = pd.crosstab(train[target], train[cat_var])
-    chi2, p, degf, expected = stats.chi2_contingency(observed)
-    print(f'Chi2: {chi2}, p-value: {p}')
-    
-    # Create a horizontal bar plot of the integer variable split by the target variable
-    plt.figure(figsize=(4,4))
-    sns.barplot(x=cat_var, y=target, data=train, alpha=.8, orient='h')
-    
-    # Add a vertical line to the plot at the overall rate of the target variable
-    overall_rate = train[target].mean()
-    plt.axvline(overall_rate, ls='--')
-    
-    # Show the plot
-    plt.show()
-
-def explore_bivariate_categorical(train, target, cat_var):
+def explore_categorical(train, target, cat_var):
     """
     Explore the relationship between a binary target variable and a categorical variable.
 
@@ -73,45 +25,22 @@ def explore_bivariate_categorical(train, target, cat_var):
     None
     """
     # Print the name of the categorical variable
-    print(cat_var, "\n_____________________\n")
-    
+    print(cat_var, "&", target)
+    print("")
+
     # Calculate the chi-squared test statistic, p-value, degrees of freedom, and expected values
     ct = pd.crosstab(train[cat_var], train[target], margins=True)
     chi2, p, dof, expected = stats.chi2_contingency(ct)
-    print(f'Chi2: {chi2}, p-value: {p}, dof: {dof}')
-    
+    print(f"Chi2: {chi2}")
+    print(f"P-value: {p}")
+    print(f"Degrees of Freedom: {dof}")
+
     # Create a count plot of the categorical variable split by the target variable
-    p = sns.catplot(x=cat_var, hue=target, data=train, kind='count')
-    p.fig.suptitle(f'{cat_var} by {target}')
-    
-    # Print the observed and expected contingency tables
-    print("\nobserved:\n", ct)
-    print("\nexpected:\n", expected)
-    
+    p = sns.catplot(x=cat_var, hue=target, data=train, kind="count")
+    p.fig.suptitle(f"{cat_var} by {target}")
+
     # Show the plot
     plt.show()
-    print("\n_____________________\n")
-    
-def explore_bivariate(train, target, cat_vars, quant_vars):
-    """
-    Explore the relationship between a binary target variable and multiple categorical and quantitative variables.
-
-    Parameters:
-    train (pandas.DataFrame): The training data.
-    target (str): The name of the binary target variable.
-    cat_vars (list of str): The names of the categorical variables to explore.
-    quant_vars (list of str): The names of the quantitative variables to explore.
-
-    Returns:
-    None
-    """
-    # Explore each categorical variable
-    for cat in cat_vars:
-        explore_bivariate_categorical(train, target, cat)
-    
-    # Explore each quantitative variable
-    for quant in quant_vars:
-        explore_bivariate_quant(train, target, quant)
 
 
 def explore_bivariate_quant(train, target, quant_var):
@@ -126,21 +55,17 @@ def explore_bivariate_quant(train, target, quant_var):
     Returns:
     None
     """
-    # Print the name of the quantitative variable
-    print(quant_var, "\n____________________")
-    
+
     # Compare the means of the quantitative variable between the two groups defined by the target variable
-    compare_means(train, target, quant_var)
-    
-    # Print a separator line
-    print("____________________")
-    
+    e.compare_means(train, target, quant_var)
+
     # Create a boxen plot of the quantitative variable split by the target variable
-    plt.figure(figsize=(4,4))
-    plot_boxen(train, target, quant_var)
-    
+    # plt.figure(figsize=(4, 4))
+    e.plot_boxen(train, target, quant_var)
+
     # Show the plot
     plt.show()
+
 
 def plot_boxen(train, target, quant_var):
     """
@@ -156,18 +81,19 @@ def plot_boxen(train, target, quant_var):
     """
     # Calculate the average value of the quantitative variable
     average = train[quant_var].mean()
-    
+
     # Create a boxen plot with the target variable on the x-axis and the quantitative variable on the y-axis
     # The plot is colored by the target variable and uses the "red" color palette
     sns.boxenplot(data=train, x=target, y=quant_var, hue=target)
-    
+
     # Add a title to the plot with the name of the quantitative variable
     plt.title(quant_var)
-    
-    # Add a horizontal line to the plot at the average value of the quantitative variable
-    plt.axhline(average, ls='--')
 
-def compare_means(train, target, quant_var, alt_hyp='two-sided'):
+    # Add a horizontal line to the plot at the average value of the quantitative variable
+    plt.axhline(average, ls="--")
+
+
+def compare_means(train, target, quant_var, alt_hyp="two-sided"):
     """
     Compare the means of a quantitative variable between two groups defined by a binary target variable using the Mann-Whitney U test.
 
@@ -181,56 +107,44 @@ def compare_means(train, target, quant_var, alt_hyp='two-sided'):
     None
     """
     # Select the values of the quantitative variable for each group
-    x = train[train[target]==1][quant_var]
-    y = train[train[target]==0][quant_var]
-    
+    x = train[train[target] == 1][quant_var]
+    y = train[train[target] == 0][quant_var]
+
     # Calculate the Mann-Whitney U test statistic and p-value
     stat, p = stats.mannwhitneyu(x, y, use_continuity=True, alternative=alt_hyp)
-    
+
     # Print the results of the test
-    print("Mann-Whitney Test:", f'stat = {stat}, p = {p}')
+    print("Mann-Whitney Test:")
+    print(f"Stat = {stat}")
+    print(f"P-Value = {p}")
 
 
-def plot_cat_by_target(train: pd.DataFrame, target: str, cat_var: str) -> plt.Axes:
+def compute_categorical_feature_importance(df, target_variable):
     """
-    Plot a bar chart of a categorical variable against a target variable, with a horizontal line indicating the overall rate of the target variable.
+    Computes the mutual information score between each categorical variable and the target variable.
 
     Parameters:
-    train (pd.DataFrame): The training dataset.
-    target (str): The name of the target variable.
-    cat_var (str): The name of the categorical variable.
+    df (pandas.DataFrame): The input DataFrame.
+    target_variable (str): The name of the target variable.
 
     Returns:
-    ax (plt.Axes): The plot object.
+    pandas.Series: A Pandas Series containing the mutual information score for each categorical variable.
     """
-    # Create a new figure with a size of 2x2 inches
-    fig = plt.figure(figsize=(2,2))
-    
-    # Create a bar plot with the categorical variable on the x-axis and the target variable on the y-axis
-    ax = sns.barplot(x=cat_var, y=target, data=train, alpha=.8)
-    
-    # Calculate the overall rate of the target variable and add a horizontal line to the plot at that level
-    overall_rate = train[target].mean()
-    ax.axhline(overall_rate, ls='--')
-    
-    # Return the plot object
-    return ax
+    # select categorical variables excluding the response variable
+    categorical_variables = df.select_dtypes(include=object).drop(
+        target_variable, axis=1
+    )
 
-def baseline(target):
-    """
-    Calculates the baseline accuracy of a classification model that always predicts the most common value of the target variable.
+    # compute the mutual information score between each categorical variable and the target
+    feature_importance = categorical_variables.apply(
+        lambda x: mutual_info_score(x, df[target_variable])
+    ).sort_values(ascending=False)
 
-    Parameters:
-    target (pandas.Series): The target variable to be predicted.
+    # visualize feature importance
+    ax = feature_importance.plot(kind="barh", figsize=(10, 8), zorder=2, width=0.8)
+    ax.set_xlabel("Importance")
+    ax.set_ylabel("Feature")
+    ax.set_title("Feature Importance (SK Learn's Mutual Info Score)")
+    plt.show()
 
-    Returns:
-    None
-    """
-    # Calculate the most common value of the target variable
-    most_common = target.value_counts().idxmax()
-    
-    # Calculate the accuracy of a model that always predicts the most common value
-    accuracy = (target == most_common).mean() * 100
-    
-    # Print the baseline accuracy as a percentage
-    print(f'Baseline: {round(accuracy, 2)}% Accuracy')
+    return feature_importance
